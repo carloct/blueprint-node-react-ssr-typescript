b373a3df2a6c2a98e8ea217a1482fb886444b957
Carlo <c.catemario@gmail.com>
2019-01-19 23:59:09 +0000

    added webpack css and assets config

--- webpack.config.base.babel.js
+++ webpack.config.base.babel.js
@@ -1,5 +1,12 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+import cssnano from 'cssnano';
+import postcssImport from 'postcss-import';
+import postcssPresetEnv from 'postcss-preset-env';
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 export default {
   context: path.resolve(__dirname),
@@ -13,7 +20,69 @@ export default {
         use: [
           { loader: 'ts-loader' }
         ]
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: !devMode,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: () => [postcssImport, postcssPresetEnv, cssnano],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: !devMode,
+              importLoaders: 2,
+              modules: true,
+              localIdentName: '[local]--[hash:base64:8]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: () => [postcssImport, postcssPresetEnv, cssnano],
+            },
+          },
+          { loader: 'sass-loader', options: { sourceMap: true }}
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|tiff)(\?.*)?$/,
+        use : [
+          { loader: 'url-loader', options: { limit: 10000, name: '[name].[hash:7].[ext]' } },
+          { loader: 'image-webpack-loader', options: { disable: devMode } },
+        ],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: [
+          { loader: 'url-loader', options: { limit: 10000, name: '[name].[hash:7].[ext]' } },
+        ],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [
+          { loader: 'url-loader', options: { limit: 10000, name: '[name].[hash:7].[ext]' } },
+        ],
+      },
     ]
   },
 
